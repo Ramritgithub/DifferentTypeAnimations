d638d82629849148cd79d6dd29516fb44ff2114b
Ramritgithub <159051362+Ramritgithub@users.noreply.github.com>
2024-02-10 19:48:04 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var score=0;
+function increase(){
+    score=score+5;
+    document.getElementById("box").textContent=score;
+}
+function Decrease(){
+    score=score-5;
+    document.getElementById("box").textContent=score;
+
+}
